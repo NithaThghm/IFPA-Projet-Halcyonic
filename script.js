bb091b7701e4664e3e81fc7a98f50249e673f92c
NithaThghm <t.nithachan@gmail.com>
2022-05-30 16:58:40 +0200

    Working fine with some improvement on the CSS. Still the visual CSS can be improved.

--- script.js
+++ script.js
@@ -2,24 +2,31 @@ let hamburger = document.getElementById('hamburger');  //Je déclare mon button
 let slideoutmenu = document.getElementById('slideout-menu'); //Je déclare le menu sur le côté
 let body = document.body; //Je déclare le body
 
-hamburger.addEventListener("click", hideshowmenu); //J'affecte une fonction au clique du boutton
+// hamburger.addEventListener("click", showmenu);
+hamburger.onclick = hideshowmenu; //J'affecte une fonction au clique du boutton
 
 function hideshowmenu() {
 
-    let position = window.getComputedStyle(body).left; //Je récupère la valeur du display du menu
+    let position = window.getComputedStyle(body).left;
+    let x = body.getBoundingClientRect().left; //Je récupère la valeur du display du menu
     
-    if (position == "0px"){
-        console.log(position);
-        body.classList.toggle("transition-toright-body");
-        slideoutmenu.classList.toggle("transition-to0px");
+    if (x == 0){
+        console.log(x);
+        body.classList.remove("transition-to0px-body");
+        slideoutmenu.classList.remove("transition-toleft-sidemenu");
+        body.classList.add("transition-toright-body");
+        slideoutmenu.classList.add("transition-to0px-sidemenu");
     }  
-    else {
-        console.log(position);
-        body.classList.toggle("transition-to0px");
-        slideoutmenu.classList.toggle("transition-toleft-sidemenu");
+    else{
+        console.log(x);
+        body.classList.remove("transition-toright-body");
+        slideoutmenu.classList.remove("transition-to0px-sidemenu");
+        body.classList.add("transition-to0px-body");
+        slideoutmenu.classList.add("transition-toleft-sidemenu");
     }
     // Si display none j'affiche le menu et j'insère une classe au body qui décale tout vers la gauche, sinon je masque le menu et je déplace le body sur la droite.
 }
+
 
 // function hideshowmenu2() {
 
