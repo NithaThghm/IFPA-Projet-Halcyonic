b83c159980fa4d4b109d11c14ff53707a7a87dc8
NithaThghm <t.nithachan@gmail.com>
2022-05-31 15:13:13 +0200

    Comments added, not used codelines deleted, added some improvements

--- script.js
+++ script.js
@@ -1,44 +1,27 @@
 let hamburger = document.getElementById('hamburger');  //Je déclare mon button
-let slideoutmenu = document.getElementById('slideout-menu'); //Je déclare le menu sur le côté
+let side_menu = document.getElementById('side-menu'); //Je déclare le menu sur le côté
 let body = document.body; //Je déclare le body
 
-// hamburger.addEventListener("click", showmenu);
 hamburger.onclick = hideshowmenu; //J'affecte une fonction au clique du boutton
 
 function hideshowmenu() {
 
     let position = window.getComputedStyle(body).left;
-    let x = body.getBoundingClientRect().left; //Je récupère la valeur du display du menu
+    let x = body.getBoundingClientRect().left; //Je récupère la valeur de la position sur l'axe X du body
     
     if (x == 0){
         console.log(x);
         body.classList.remove("transition-to0px-body");
-        slideoutmenu.classList.remove("transition-toleft-sidemenu");
+        side_menu.classList.remove("transition-toleft-sidemenu");
         body.classList.add("transition-toright-body");
-        slideoutmenu.classList.add("transition-to0px-sidemenu");
+        side_menu.classList.add("transition-to0px-sidemenu");
     }  
     else{
         console.log(x);
         body.classList.remove("transition-toright-body");
-        slideoutmenu.classList.remove("transition-to0px-sidemenu");
+        side_menu.classList.remove("transition-to0px-sidemenu");
         body.classList.add("transition-to0px-body");
-        slideoutmenu.classList.add("transition-toleft-sidemenu");
+        side_menu.classList.add("transition-toleft-sidemenu");
     }
-    // Si display none j'affiche le menu et j'insère une classe au body qui décale tout vers la gauche, sinon je masque le menu et je déplace le body sur la droite.
+    // Si le body est en 0, j'insère les classes nécessaires pour afficher le menu, sinon je masque le menu : l'ensemble du body se déplace sur la droite et la gauche.
 }
-
-
-// function hideshowmenu2() {
-
-//     let display = window.getComputedStyle(slideoutmenu).display; //Je récupère la valeur du display du menu
-
-//     if (display == "none"){
-//         // slideoutmenu.style.display = "block";
-//         body.classList.toggle("body-push-toright");
-//     }  
-//     else {
-//         // slideoutmenu.style.display = "none";
-//         body.classList.toggle("body-push-toright");
-//     }
-//     // Si display none j'affiche le menu et j'insère une classe au body qui décale tout vers la gauche, sinon je masque le menu et je déplace le body sur la droite.
-// }
